62ec606c43092e0f2535034e5a6c8292771d365a
T. Bunratta <tharathorn.bu@ku.th>
2019-08-31 10:58:57 +0700

    Provide user-specific registry config

--- src/auto_config.bat
+++ src/auto_config.bat
@@ -14,3 +14,8 @@ set CURR_USER=STIN-%NUM%
 echo %CURR_USER%
 echo "Adding user... %CURR_USER%"
 net user %CURR_USER% %CURR_PASS% /add
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d %CURR_USER% /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d %CURR_PASS% /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Installer" /v DisableMSI /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Policies\Microsoft\Windows\Installer" /v AlwaysInstallElevated /t REG_DWORD /d 0 /f
