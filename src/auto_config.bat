f9a044cb679b0df7d73f0a07eda212eb669721e7
T. Bunratta <tharathorn.bu@ku.th>
2019-08-31 10:46:12 +0700

    Provide a backup drive selection for a user of the batch file

--- src/auto_config.bat
+++ src/auto_config.bat
@@ -3,3 +3,7 @@ title "Auto Config by T. Bunratta. | 2019"
 echo ==========
 echo "Auto Config by T. Bunratta. | 2019"
 echo ==========
+
+set /p LETTER="Your USB Backup Drive letter (without a colon): "
+%LETTER%:
+cd %LETTER%:\FileMaster\SID
